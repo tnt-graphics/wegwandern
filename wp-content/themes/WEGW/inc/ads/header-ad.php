35e3a9f1761189ce00a0c4bf49f1e72e57977ddb
rilanatnt <martinato@tnt-graphics.ch>
2025-07-09 10:09:19 +0200

    file from live

--- wp-content/themes/WEGW/inc/ads/header-ad.php
+++ wp-content/themes/WEGW/inc/ads/header-ad.php
@@ -13,11 +13,11 @@
 <div class='ad-above-header-container header-ad-desktop-wrapper'>
 	<div class="ad-section header"></div>
 
-	<!-- <div class="close_ad" onclick="adCloseHeader()"><span class="close_ad_icon"></span></div> -->
+	<!-- <div class="close_ad" onclick="adCloseHeader()"><span class="close_ad_icon"></span></div> 
 	<div class="ad-scroll-btn">
 		<div class="ad-scroll-btn-icon"></div>
 		<div class="ad-scroll-btn-text" onclick="adScrollToHeader()">Scrolle zu WegWandern.ch</div>
-	</div>
+	</div> -->
 </div>
 
 <script type="text/javascript">
